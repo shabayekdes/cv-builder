d7e482edcc831c282b51d0168ee24c4117bad0c0
Esmail Shabayek <esmail.shabayek@gmail.com>
2021-02-27 01:36:23 +0200

    Add candidate model and migrations

--- database/seeders/CandidateSeeder.php
+++ database/seeders/CandidateSeeder.php
@@ -4,17 +4,16 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CandidateSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            CandidateSeeder::class,
-        ]);
+        \App\Models\Candidate::factory(10)->create();
+
     }
 }
