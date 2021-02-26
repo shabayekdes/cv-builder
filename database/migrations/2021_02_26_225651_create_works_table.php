a6301365c85549e348578a80bd5e94c326eb6882
Esmail Shabayek <esmail.shabayek@gmail.com>
2021-02-27 01:45:25 +0200

    Add works model and migrations

---
+++ database/migrations/2021_02_26_225651_create_works_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('company_name');
+            $table->string('location');
+            $table->integer('employment_type');
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->boolean('is_current_position')->default(0);
+            $table->text('description')->nullable();
+            $table->timestamps();
+
+            $table->foreignId('candidate_id')
+                    ->constrained()
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
